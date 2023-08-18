e2e59324f0cbb24c2a4067d20b0273c46a7f6e17
Alexander Hergert <alexander.hergert1989@gmail.com>
2023-08-18 16:30:25 +0200

    Create global context for darkMode.

---
+++ src/context.jsx
@@ -0,0 +1,22 @@
+import { useContext, createContext, useState } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  return (
+    <AppContext.Provider
+      value={{
+        isDarkMode,
+        setIsDarkMode,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
